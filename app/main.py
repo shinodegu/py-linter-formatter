febb91897306e87fcfec3ca2048e0e9d123998d9
Ihor <andreiunderwhat@mail.com>
2024-10-08 23:20:00 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -4,7 +4,8 @@ def format_linter_error(error: dict) -> dict:
         "column": error["column_number"],
         "message": error["text"],
         "name": error["code"],
-        "source": "flake8"}
+        "source": "flake8"
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
@@ -13,5 +14,5 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(key, value)
-            for key, value in linter_report.items()]
+    return [format_single_linter_file(report_path, errors)
+            for report_path, errors in linter_report.items()]
